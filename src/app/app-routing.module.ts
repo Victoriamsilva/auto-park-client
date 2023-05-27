49b74d7da7df84caf70f08dd7709b71dcff379e8
Victoriamsilva <victoria.marques8424@gmail.com>
2023-05-27 20:49:51 -0300

    authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authenticationGuard } from './guards/authentication.guard';
 
-const routes: Routes = [{ path: 'entrance', loadChildren: () => import('./pages/entrance/entrance.module').then(m => m.EntranceModule) }, { path: 'exit', loadChildren: () => import('./pages/exit/exit.module').then(m => m.ExitModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '',
+    canActivate: [authenticationGuard],
+    children: [
+      {
+        path: 'entrance',
+        loadChildren: () =>
+          import('./pages/private/entrance/entrance.module').then(
+            (m) => m.EntranceModule
+          ),
+      },
+      {
+        path: 'exit',
+        loadChildren: () =>
+          import('./pages/private/exit/exit.module').then((m) => m.ExitModule),
+      },
+    ],
+  },
+  {
+    path: 'authentication',
+    loadChildren: () =>
+      import('./pages/public/authentication/authentication.module').then(
+        (m) => m.AuthenticationModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
